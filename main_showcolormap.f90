1ff253d6d550d0455af92e904a0a3af24222a92d
Zdenek Grof <grofz@vscht.cz>
2025-07-19 13:02:53 +0200

    Add viridis colormap

--- main_showcolormap.f90
+++ main_showcolormap.f90
@@ -4,7 +4,7 @@ program example_colormap
   implicit none
 
   integer, parameter :: WIDTH = 300, HEIGHT = 99
-  integer, parameter :: COLORMAP_SIZE = 10
+  integer, parameter :: CM_SIZE = 11 ! 9+2 colours 
 
   integer, dimension(WIDTH, HEIGHT) :: rr, gg, bb
   real(DP) :: u(WIDTH, HEIGHT)
@@ -15,12 +15,16 @@ program example_colormap
     u(i,:) = real(i-1)/real(size(u,1)-1)
   end do
 
-  ! artificial rectangles
+  ! artificial rectangles illustrating out-of-range values
   u(1:WIDTH/10, 1:HEIGHT/3) = 2.0_DP
   u(9*WIDTH/10+1:WIDTH, 2*HEIGHT/3+1:HEIGHT) = -1.0_DP
 
-  call assign_colormap(u, rr, gg, bb, COLORMAP_SIZE, 0.0_DP, 1.0_DP)
-  call writeppm('test_colormap.ppm', rr, gg, bb)
-  call writeppm('test_colormap_plain.ppm', rr, gg, bb, is_plain=.true.)
+  call assign_colormap(u, rr, gg, bb, CM_SIZE, 0.0_DP, 1.0_DP, CM_RAINBOW)
+  call writeppm('test_colormap1.ppm', rr, gg, bb)
+  call writeppm('test_colormap1_plain.ppm', rr, gg, bb, is_plain=.true.)
+
+  call assign_colormap(u, rr, gg, bb, CM_SIZE, 0.0_DP, 1.0_DP, CM_VIRIDIS)
+  call writeppm('test_colormap2.ppm', rr, gg, bb)
+ !call writeppm('test_colormap2_plain.ppm', rr, gg, bb, is_plain=.true.)
 
 end program example_colormap
